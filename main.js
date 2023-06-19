f4a42a78d6bfe8404d6df9c7e77c445626f86459
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2023-06-19 18:08:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 async function dadosJson () {
   const dados = await fetch('./data.json');
   components = await dados.json();
+
+  components.forEach((component) => {
+    console.log(component.category)
+    console.log(component.icon)
+    console.log(component.score)
+  })
 }
+
+dadosJson()
