529f2d5aa60db325bbc12dfb8784140f47a267c8
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2023-06-19 18:54:29 -0300

    finalmente

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+const gerarLista = document.querySelector('[data-list]')
+const gerarScore = document.querySelector('[data-results]')
+let auxiliar = 0
+let media = 0
+let listaDeDados = []
 async function dadosJson () {
-  const dados = await fetch('./data.json');
-  components = await dados.json();
+  listaDeDados = await fetch('./data.json');
+  components = await listaDeDados.json();
 
   components.forEach((component) => {
-    console.log(component.category)
-    console.log(component.icon)
-    console.log(component.score)
+
+    auxiliar = auxiliar += component.score
+    gerarLista.innerHTML += `<li class="${component.category}
+    list"><p>${component.category}</p><span class="info"><strong>
+    ${component.score}</strong><span>/ 100</span></span></li>`
+    
   })
+  media = auxiliar / components.length
+  
+  gerarScore.innerHTML += `<div class="results-title">
+  <h1>Your Result</h1>
+  </div>
+  <div class="results-circle">
+  <p><strong>${media.toFixed(0)}</strong></p>
+  <p>of 100</p>
+  </div>
+  <div class="results-text">
+  <h1>Great</h1>
+  <p>You scored higher than 65% of the people who have taken these tests.</p>
+  </div>`
 }
 
 dadosJson()
